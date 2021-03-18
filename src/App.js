9b3d4aa39e4953b68c9c0b486cdff12dcfe23a46
RTEYL <tyler-w-jones@hotmail.com>
2021-03-17 22:51:09 -0700

    add dynamic background images

--- src/App.js
+++ src/App.js
@@ -9,46 +9,76 @@ import {
   NavLink,
   Redirect,
 } from "react-router-dom";
+import { makeStyles } from "@material-ui/styles";
 const content = require("./content.json").pages;
+
+const useStyles = makeStyles({
+  app: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    maxWidth: "100vw",
+    minHeight: "100vh",
+    paddingLeft: "5%",
+    paddingRight: "5%",
+  },
+  container: {
+    maxWidth: "1440px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  li: {
+    listStyle: "none",
+  },
+  btn: {
+    backgroundColor: "transparent",
+    color: "white",
+    border: ".1rem solid white",
+  },
+});
 
 const App = () => {
   const getDataBySlug = (slug) => {
     return content.filter((page) => page.slug === slug)[0];
   };
+  const classes = useStyles();
 
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <img src={Logo} alt="abc logo" />
-            <li>
-              <NavLink
-                aria-current="true"
-                activeStyle={{ color: "orangered" }}
-                to="/industries">
-                Industries
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                aria-current="true"
-                activeStyle={{ color: "orangered" }}
-                to="/services">
-                Services
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                aria-current="true"
-                activeStyle={{ color: "orangered" }}
-                to="/about-us">
-                About Us
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <button>Contact Us</button>
+      <div className={classes.app}>
+        <div className={classes.container}>
+          <nav>
+            <ul className={classes.li}>
+              <img src={Logo} alt="abc logo" />
+              <li>
+                <NavLink
+                  aria-current="true"
+                  activeStyle={{ color: "orangered" }}
+                  to="/industries">
+                  Industries
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  aria-current="true"
+                  activeStyle={{ color: "orangered" }}
+                  to="/services">
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  aria-current="true"
+                  activeStyle={{ color: "orangered" }}
+                  to="/about-us">
+                  About Us
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <button className={classes.btn}>Contact Us</button>
+        </div>
         <Switch>
           <Route path="/industries">
             <Marquee data={getDataBySlug("industries")} />
--- src/marquee.js
+++ src/marquee.js
@@ -1,13 +1,40 @@
+import { makeStyles } from "@material-ui/styles";
 import React from "react";
 import Arrow from "./arrow.svg";
 
+const useStyles = makeStyles({
+  headline: (props) => ({
+    backgroundImage: `url(./${props.background})`,
+    width: "45%",
+  }),
+  subhead: {
+    width: "45%",
+  },
+  container: {
+    maxWidth: "1440px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center",
+    color: "white",
+    wordBreak: "break-all",
+  },
+  cta: {
+    backgroundColor: "white",
+    color: "black",
+  },
+});
+
 const Marquee = ({ data }) => {
+  const classes = useStyles({ ...data.blocks[0] });
+
   return (
     <>
-      <h1>{data.title}</h1>
-      <p>{data.blocks[0].subhead}</p>
-      <div>
-        <h3>{data.blocks[0].cta}</h3>
+      <div className={classes.container}>
+        <h1 className={classes.headline}>{data.blocks[0].headline}</h1>
+        <p className={classes.subhead}>{data.blocks[0].subhead}</p>
+      </div>
+      <div className={`${classes.cta} ${classes.container}`}>
+        <p style={{ fontWeight: "bold" }}>{data.blocks[0].cta}</p>
         <p>
           LET"S TALK. <img src={Arrow} alt="arrow-icon" />
         </p>
